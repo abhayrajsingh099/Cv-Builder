e49968013f1e4d4e5caa3c6f22ad8af17a2c9a1f
abhayrajsingh099 <abhayrajsingh2091@gmail.com>
2025-07-02 23:33:14 +0530

    changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-30^&8n6!0uyv%iqw-)my!4n_mh1*^$g^ff07bc6b0rb8_*pv4_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
